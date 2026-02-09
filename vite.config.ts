6eb127eb43a9eb1d09bec19ee4c248d18731b1ee
mohd <mohd@gmail.com>
2026-02-09 21:30:26 +0500

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
+console.log('GITHUB_PAGES', process.env.GITHUB_PAGES);
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
