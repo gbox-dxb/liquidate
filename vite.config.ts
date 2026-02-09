bf4cb50ab1b94a1f2bbf0181c960c829d37f6ef9
mohd <mohd@gmail.com>
2026-02-09 21:01:09 +0500

    fixed github pages build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.GITHUB_PAGES === "true" ? "/liquidate/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
